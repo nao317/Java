a97bc24e3292e99233d1ded3366091b014d8d65a
nao317 <country.gentleman.0317@gmail.com>
2024-12-27 13:44:57 +0900

    commit from JetBrains-IntelliJ

---
+++ atcoder/abc/abc379_a.java
@@ -0,0 +1,11 @@
+/* abc379_a.java */
+
+import java.util.*;
+
+public class abc379_a {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String n = scan.nextInt();
+        int a = 
+    }
+}
---
+++ atcoder/otherContest/tessoku/A01.java
@@ -0,0 +1,13 @@
+/* The First Problem */
+
+import java.util.*;
+
+public class A01 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int result = n * n;
+        System.out.println(result);
+        scan.close();
+    }
+}
